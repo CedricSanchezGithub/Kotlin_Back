4e8a99bb6cc67618ebd44fb35cd5661ccd2376e8
CedricSanchezGithub <129597649+CedricSanchezGithub@users.noreply.github.com>
2024-10-21 20:45:56 +0200

    Update and rename UserEntity.kt to NetflixEntity.kt

---
+++ src/main/kotlin/org/example/kotlin_back/entity/NetflixEntity.kt
@@ -0,0 +1,13 @@
+@Entity
+@Table(name = "netflix")
+data class NetflixEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column(name = "movie", nullable = false)
+    var movie: String? = "aucune donnée pour movie",
+
+    @Column(name = "country", nullable = false)
+    var country: String? = "aucune donnée pour country",
+    }
