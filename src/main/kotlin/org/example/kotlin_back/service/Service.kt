5718e5f4e476becfaa848c35f24163b7d10fabf0
CedricSanchezGithub <129597649+CedricSanchezGithub@users.noreply.github.com>
2024-10-21 20:43:32 +0200

    Update Service.kt

--- src/main/kotlin/org/example/kotlin_back/service/Service.kt
+++ src/main/kotlin/org/example/kotlin_back/service/Service.kt
@@ -1,11 +1,20 @@
 package org.example.kotlin_back.service
 
+import org.example.kotlin_back.entity.NetflixEntity
 import org.example.kotlin_back.repository.NetflixRepository
 import org.springframework.stereotype.Service
 
 @Service
-class NetflixService(val netflixRepository: NetflixRepository) {
+class NetflixService(
+    val netflixRepository: NetflixRepository
+) {
 
+    fun readAllMovies(): List<NetflixEntity> {
+        return netflixRepository.findAll()
+    }
 
+    fun postAllMovies(movies: List<NetflixEntity>) {
+        netflixRepository.saveAll(movies)
+    }
 
 }
