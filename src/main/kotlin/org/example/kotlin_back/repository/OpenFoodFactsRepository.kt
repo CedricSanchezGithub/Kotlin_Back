87bcee26a144ad841ba30fbf23ad9760161825e2
CedricSanchezGithub <mycedmail@gmail.com>
2024-11-17 14:46:33 +0100

    openfood maj & docker pipline

--- src/main/kotlin/org/example/kotlin_back/repository/OpenFoodFactsRepository.kt
+++ src/main/kotlin/org/example/kotlin_back/repository/OpenFoodFactsRepository.kt
@@ -1,7 +1,7 @@
 package org.example.kotlin_back.repository
 
-import org.example.kotlin_back.entity.NetflixEntity
+import org.example.kotlin_back.entity.OpenFoodFactsEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository interface NetflixRepository : JpaRepository<NetflixEntity, String>
+@Repository interface OpenFoodFactsRepository : JpaRepository<OpenFoodFactsEntity, String>
